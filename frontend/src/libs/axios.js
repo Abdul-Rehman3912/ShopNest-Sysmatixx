389bb02423f055c6b6360c213bf626a3d9802585
Abdul Rehman <abdulrahmankhan.8x@gmail.com>
2026-07-04 12:28:16 +0500

    Url update in axios

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000' 
-  : 'https://shop-nest-sysmatixx.vercel.app'; 
+  : 'https://shop-nest-sysmatixx-ijjl5s8bj-abdul-rehman2661s-projects.vercel.app'; 
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
